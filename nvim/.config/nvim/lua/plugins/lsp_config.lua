057c6d581713248f7319758388a92eda28bb9b8b
Marcin Fudala <nerko937@gmail.com>
2024-07-12 11:10:36 +0200

    added mason and updated plugins

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -1,27 +1,42 @@
 return {
     {
-        "neovim/nvim-lspconfig",
-        lazy = false,
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
         config = function()
-            lspconfig = require("lspconfig")
-            lspconfig.pylsp.setup {
-                on_attach = function(client)
-                    vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {})
-                    vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {})
-                end,
-                settings = {
-                    pylsp = {
-                        plugins = {
-                            pycodestyle = {ignore = {"W391", "W503"}, maxLineLength = 120}
-                        }
-                    }
-                }
-            }
-            lspconfig.rust_analyzer.setup({
-                on_attach = function(client)
-                    vim.keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>", {})
-                    vim.keymap.set("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", {})
-                end,
+            local lsp_zero = require('lsp-zero')
+            lsp_zero.extend_lspconfig()
+            lsp_zero.on_attach(function(client, bufnr)
+                -- see :help lsp-zero-keybindings
+                -- to learn the available actions
+                lsp_zero.default_keymaps({buffer = bufnr})
+            end)
+        end
+    },
+    { 'neovim/nvim-lspconfig' },
+    {
+        'williamboman/mason.nvim',
+        dependencies = { 'VonHeikemen/lsp-zero.nvim' },
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { 'VonHeikemen/lsp-zero.nvim' },
+        config = function()
+            require('mason').setup({})
+            require('mason-lspconfig').setup({
+                ensure_installed = {
+                    "jedi_language_server",
+                    "ruff_lsp",
+                    "lua_ls",
+                    "rust_analyzer"
+                },
+                handlers = {
+                    function(server_name)
+                        require('lspconfig')[server_name].setup({})
+                    end,
+                },
             })
         end
     },
