4d34006ff1f7e59cefe1e59f7558095390dc13dd
Marcin Fudala <nerko937@gmail.com>
2025-02-25 08:49:48 +0100

    Added python-import

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -41,4 +41,87 @@ return {
         event = "InsertEnter",
         config = true
     },
+    {
+        "kiyoon/python-import.nvim",
+        build = "uv tool install . --force --reinstall",
+        keys = {
+            {
+                "<M-CR>",  -- alt + enter
+                function()
+                    require("python_import.api").add_import_current_word_and_notify()
+                end,
+                mode = { "i", "n" },
+                silent = true,
+                desc = "Add python import",
+                ft = "python",
+            },
+            {
+                "<M-CR>",
+                function()
+                    require("python_import.api").add_import_current_selection_and_notify()
+                end,
+                mode = "x",
+                silent = true,
+                desc = "Add python import",
+                ft = "python",
+            },
+            {
+                "<space>i",
+                function()
+                    require("python_import.api").add_import_current_word_and_move_cursor()
+                end,
+                mode = "n",
+                silent = true,
+                desc = "Add python import and move cursor",
+                ft = "python",
+            },
+            {
+                "<space>i",
+                function()
+                    require("python_import.api").add_import_current_selection_and_move_cursor()
+                end,
+                mode = "x",
+                silent = true,
+                desc = "Add python import and move cursor",
+                ft = "python",
+            },
+            {
+                "<space>tr",
+                function()
+                    require("python_import.api").add_rich_traceback()
+                end,
+                silent = true,
+                desc = "Add rich traceback",
+                ft = "python",
+            },
+        },
+        opts = {
+            extend_lookup_table = {
+                ---@type string[]
+                import = {
+                    -- "tqdm",
+                },
+
+                ---@type table<string, string>
+                import_as = {
+                    -- These are the default values. Here for demonstration.
+                    -- np = "numpy",
+                    -- pd = "pandas",
+                },
+
+                ---@type table<string, string>
+                import_from = {
+                    timezone = "django.utils",
+                    Pattern = "apps.allsafe.models",
+                    -- tqdm = nil,
+                    -- tqdm = "tqdm",
+                },
+
+                ---@type table<string, string[]>
+                statement_after_imports = {
+                    -- logger = { "import my_custom_logger", "", "logger = my_custom_logger.get_logger()" },
+                },
+            },
+        },
+    },
 }
