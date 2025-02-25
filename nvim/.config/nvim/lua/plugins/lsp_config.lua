73aae587183b533e8863176308d4a3427b207d50
Marcin Fudala <nerko937@gmail.com>
2025-02-25 08:56:57 +0100

    Changed python lsp to jedi and updated plugins

--- nvim/.config/nvim/lua/plugins/lsp_config.lua
+++ nvim/.config/nvim/lua/plugins/lsp_config.lua
@@ -27,21 +27,60 @@ return {
             require('mason').setup({})
             require('mason-lspconfig').setup({
                 ensure_installed = {
+                    "jedi_language_server",
                     "lua_ls",
-                    "pyright",
-                    "rust_analyzer"
                 },
                 handlers = {
                     function(server_name)
                         require('lspconfig')[server_name].setup({})
                     end,
-                    pyright = function()
-                        require('lspconfig').pyright.setup({
-                            before_init = function(_, config)
-                                config.settings.python.pythonPath = "./.venv/bin/python"
-                                config.settings.python.analysis.typeCheckingMode = "off"
-                            end
-                        })
+                    jedi_language_server = function()
+                        require('lspconfig').jedi_language_server.setup {
+                            init_options = {
+                                workspace = {
+                                    environmentPath = "./.venv/bin/python"
+                                },
+                                completion = {
+                                    disableSnippets = true
+                                }
+                            }
+                        }
+                    end,
+                    lua_ls = function ()
+                        require'lspconfig'.lua_ls.setup {
+                            on_init = function(client)
+                                if client.workspace_folders then
+                                    local path = client.workspace_folders[1].name
+                                    if vim.uv.fs_stat(path..'/.luarc.json') or vim.uv.fs_stat(path..'/.luarc.jsonc') then
+                                        return
+                                    end
+                                end
+
+                                client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+                                    runtime = {
+                                        -- Tell the language server which version of Lua you're using
+                                        -- (most likely LuaJIT in the case of Neovim)
+                                        version = 'LuaJIT'
+                                    },
+                                    -- Make the server aware of Neovim runtime files
+                                    workspace = {
+                                        checkThirdParty = false,
+                                        library = {
+                                            vim.env.VIMRUNTIME,
+                                            "${3rd}/love2d/library",
+                                            -- Depending on the usage, you might want to add additional paths here.
+                                            -- "${3rd}/luv/library"
+                                            -- "${3rd}/busted/library",
+                                        }
+                                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower and will cause issues when working on your own configuration (see https://github.com/neovim/nvim-lspconfig/issues/3189)
+                                        -- library = vim.api.nvim_get_runtime_file("", true)
+                                    }
+                                })
+                            end,
+                            settings = {
+                                Lua = {}
+                            }
+                        }
                     end
                 },
             })
